5a87cb646d74529a3a9e87d6bff0ddd71e55b81c
chloetgy <50623722+chloetgy@users.noreply.github.com>
2019-06-04 18:03:29 +0200

    Cours 4
    Iteration, DOM

--- script.js
+++ script.js
@@ -1,129 +1,186 @@
 'use strict';
 
-/* Arrays (Tableaux)*/
+/*let myArr = [1, 2, 3];
 
-let username1 = "Jeremy";
-let username2 = "Toto";
-let username3 = "Gio";
+let result = 0;
 
-let usernames = ["Jeremy", "Toto", "Gio"];
-console.log('usernames', usernames);
+myArr.forEach(function(element) {
+let res = 0;
+debugger;
+result = result + element;
+})*/
 
-usernames[0] // premier élément
-usernames[1] // deuxième élément
-usernames[2] // troisième élément
+/* EXERCICE 1*/
 
-let usernamesLength = usernames.length // 3
-usernames [usernamesLength] // undefined
-usernames [usernames.length - 1] // troisième élément
+let dividedNumbers = [] ;
+let numbers = [0, 1, 2, 3, 4, 5];
+let result = [0];
 
-let websites = ["LinkedIn", "Behance", "Facebook"];
-console.log('websites', websites);
-
-let websitesLength = websites.length // 3
-console.log('First website', websites[0])
-console.log('Second website', websites[1])
-console.log('Last website', websites [websites.length - 1])
-console.log('List length', websites.length)
-
-let myArray = [[1, 2, 3], null, undefined, 2, [4,2,3]];
-console.log('Premier élément du premier élément', myArray[0][0]);
-
-/* EXERCICE: Création d'un tableau qui contient 3 tableaux.*/
-
-let myTable = [[1,2,3], [4,5,6], [7,8,9]];
-console.log('Deuxième élément du premier élément', myTable [0][1])
-console.log('Dernier élément du dernier élément', myTable [myTable.length - 1] [myTable.length - 1])
-
-/*shift, unshift, push, pop, concat*/
-
-const myArr = ['Hello', 'Bonjour', 'Hola'];
-console.log(myArr);
-
-// push (ajouter un élément à la fin)
-myArr.push('Hallo');
-console.log('après le push', myArr)
-
-// unshift (ajouter un élément au début du tableau)
-myArr.unshift('Nihao');
-console.log('après le unshift',myArr);
-
-// pop (supprimer le dernier élément)
-myArr.pop(); //ne prend jamais de paramètres
-// shift (supprimer le premier élément)
-myArr.shift(); //ne prend jamais de paramètres
-console.log('RESULT: myArr',myArr);
-
-/* Ajouter/supprimer les éléments du tableau*/
-
-const myNumbers = [1, 2, 3, 4, 5, 6, 7];
-console.log(myNumbers);
-myNumbers.shift();
-console.log('Supprimez le premier élément du tableau',myNumbers);
-myNumbers.pop();
-console.log('Supprimez le dernier élément du tableau',myNumbers);
-myNumbers.unshift(10);
-console.log('Ajoutez 10 au début',myNumbers);
-myNumbers.push(100);
-console.log('Ajouter 100 à la fin', myNumbers);
-
-// concat (fusionner des tableaux)
-let arr1 = [1, 2, 3]
-let arr2 = [4, 5, 6]
-let result = arr1.concat(arr2);
-console.log('concat result', result);
-
-/* EXERCICE: Fusionner plusieurs tableaux dans un seul */
-let table1 = [1, 2, 3];
-let table2 = [4, 5, 6];
-let table3 = [7, 8, 9, 10];
-let table4 = [11, 12, 13, 14];
-let resultTable = table1.concat(table2).concat(table3).concat(table4);
-console.log('concat result Table', resultTable)
-
-/* split transforme string en tableau */
-let myString = 'Bonjour!';
-// console.log(myString.split()) // ['B', 'o', ...]
-let myArr3 = myString.split(''); // mettre guillemets vides pour séparer chaque caractère
-// reverse (inverser un tableau)
-let reversedArr = myArr3.reverse();
-console.log('reversedArr', reversedArr);
-// join (transformer un tableau en une chaine de caractères)
-let res = reversedArr.join('');
-console.log('res',res);
-
-/* EXERCICE: Transformation du Tableau */
-// enchainer les méthodes : reversedArr.reverse().join()
-let welcomeMessage = 'Hello World';
-let resultat = welcomeMessage.split('').reverse().join('');
-console.log('resultat', resultat);
-
-// parcourir (donner toutes les valeurs) un tableau
-// map ou forEach (même chose)
-let myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-myNum.forEach((elem) => {
-  console.log('my array element', elem);
+numbers.forEach(function(elem) {
+result = elem / 2 ;
+dividedNumbers.push(result);
 })
 
-/*EXERCICE 1: Parcourir un tableau*/
+console.log(dividedNumbers);
 
-let myMessage = ['What', 'A', 'Wonderful', 'World'];
-myMessage.forEach((elem) => {
-  console.log('my message is', elem)
-})
+/* EXERCICE 2*/
 
-/*EXERCICE 2*/
-let myTest2 = '';
-let myTest = ['Hello ', 'World ', 'I ', 'am ', 'John ', 'Doe '];
-myTest.forEach((elem) => { // intérieur de la boucle // elem est une variable dynamique
-  myTest2 += elem;
-})
-console.log(myTest2);
+let myTab = [1, -2, 3, 4, -9, 6];
+let myTabPlus = [] ;
 
-/*EXERCICE 3*/
-let mySum = '';
-let myExercice = [0, 1, 2, 3, 4, 5];
-myExercice.forEach((elem) => {
-mySum =+ elem;
+myTab.forEach(function(elem) {
+  if (elem > 0) {
+  myTabPlus.push(elem);
+  }
 })
-console.log(mySum);
+console.log(myTabPlus);
+
+/* EXERCICE 3*/
+
+let myCelcius = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let myFinalResult = [0];
+let myFahrenheit = [];
+
+myCelcius.forEach(function(elem) {
+  myFinalResult = (elem * 9/5) + 32;
+  myFahrenheit.push(myFinalResult);
+})
+console.log(myFahrenheit);
+
+/* OBJECTS
+let user = {
+  username: 'Sasha',
+  type: 'Human',
+  age: 23,
+  friend: {
+    name: 'Claire'
+  }
+}
+
+// Propriété : Username
+// Clef de propriété : 'username' (toujours une chaine de caractères)
+// Valeur de propriété : 'Sasha'
+
+console.log(user.username) // Sasha
+const objKey = 'type'
+console.log(user[objKey]); // Human
+console.log(user['type']); // Human */
+
+/* EXERCICE 1 */
+
+let user = {
+  id : 3654,
+  name : 'TANGUY Chloe',
+  car : {
+    id: 'AA-356-AA',
+    brand : 'Peugeot',
+    color: 'Red',
+  }
+}
+console.log(user.id);
+console.log(user.car.id);
+console.log(user.car.color);
+
+/* EXERCICE 2 */
+
+let musician = {
+  firstname : 'Hans Florian',
+  lastname : 'Zimmer',
+  surname : 'Hans Zimmer',
+  albums : [
+  {
+  title: 'Spirit: Stallion of the Cimarron',
+  year: 2002,
+  numberSongs: '8',
+  trackListing: ['Here I Am', 'I Will Always Return', 'This Is Where I Belong', 'Run Free', 'Homeland (Main Title)', 'Rain', 'Nothing I have Ever Known', 'I Will Always Return (Finale)'],
+  },
+  {
+  title: 'Inception',
+  year: 2010,
+  numberSongs: '12',
+  trackListing: ['Half Remembered Dream', 'We Built Our Own World', 'Dream Is Collapsing', 'Radical Notion', 'Old Souls', '528491', 'Mombasa', 'One Simple Idea', 'Dream Within A Dream', 'Waiting For A Train', 'Paradox', 'Time'],
+  }
+]
+}
+
+console.log('Prénom du musicien :', musician.firstname);
+console.log('Titre du premier album :',musician.albums[0].title);
+console.log('Cinquième chanson de la Track List de Inception :',musician.albums[1].trackListing[4]);
+
+
+/* EXERCICE 3 */
+
+let cat = {
+  name : 'Cookie',
+  favoritePlaces: ['Random box', 'Laps', 'Christmas tree'],
+  petMyCat: function () {
+    console.log('Mrrr',petMyCat);
+  }
+}
+
+console.log('Premier endroit préféré :',cat.favoritePlaces[0]);
+
+/* EXERCICE 4 */
+
+const myPets = [
+    {
+      pet: 'cat',
+      name: 'Cookie',
+      occupation: 'sleeping all day',
+      says: () => { 'Miaou!' }
+    },
+    {
+      pet: 'dog',
+      name: 'Toffee',
+      occupation: 'walking in the park',
+      says: () => { 'Wouf!' }
+    },
+    {
+      pet: 'parrot',
+      name: 'Kesha',
+      occupation: 'mocking other pets',
+      says: () => { 'Hehe, that was fun!' }
+    }
+  ]
+
+  myPets.forEach(function(pet) {
+    console.log(`My pet ${pet.pet} named ${pet.name} loves ${pet.occupation} and keeps saying ${pet.says()}`);
+  })
+
+/*DOM*/
+
+console.log(window);
+
+  // par le nom de la balise (liste de nodes/noeuds)
+  document.getElementsByTagName('p');
+  document.querySelectorAll('p');
+
+  // par le nom de classe (liste)
+  document.getElementsByClassName('my-class');
+  document.querySelectorAll('.my-class');
+
+  // par id (node)
+  document.getElementById('my-id');
+  document.querySelector('#my-id');
+
+  let myBody = document.getElementsByTagName('body');
+
+  console.log('myBody',myBody);
+
+  /*EXERCICE 1: TROUVER UNE NODE*/
+
+  const myParagraphs = document.getElementsByTagName('p');
+  console.log('Paragraphes',myParagraphs);
+  document.querySelectorAll('p');
+
+  const myDiv = document.getElementsByClassName('circle');
+  console.log('Divisions',myDiv);
+  document.querySelectorAll('.circle');
+
+  const myBlueDiv = document.getElementById('middle-circle');
+  console.log('Paragraphes',myBlueDiv);
+  document.querySelector('#middle-circle');
+
+  const myParagraphSpan = document.getElementsByTagName('p.span');
+  console.log(myParagraphSpan);
+  document.querySelectorAll('p.span');
